709172267411ab387c051649f605f0801a0b3bfa
Nayan Kabir <nayan.kabir@utoronto.ca>
2015-09-30 03:17:10 -0400

    a

---
+++ js/randomBackground.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+var random_images_array = ["images/mountain1.jpg", "images/bg.jpg" ];
+
+function getRandomImage(imgAr, path) {
+    path = path || 'images/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "">';
+    document.write(imgStr); document.close();
+}
+
